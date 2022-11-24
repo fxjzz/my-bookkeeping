e562fc97ff0eb7688948385502a2c9299ca09a3d
fxjzz <1042183935@qq.com>
2022-11-24 17:22:19 +0800

    删除无用文件1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/my-bookkeeping/dist/',
+  // base:'/my-bookkeeping/dist/',
   plugins: [vue()]
 })
