4edba24fd25551b558639e2dfb1c7a2ede8f48f5
fxjzz <1042183935@qq.com>
2022-11-30 16:34:45 +0800

    制作 Navbar 组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -3,6 +3,7 @@ import { Button } from "../shared/Button";
 import { Center } from "../shared/Center";
 import { FloatButton } from "../shared/FloatButton";
 import { Icon } from "../shared/Icon";
+import { Navbar } from "../shared/Navbar";
 import s from "./StartPage.module.scss";
 
 export const StartPage = defineComponent({
@@ -12,13 +13,18 @@ export const StartPage = defineComponent({
     }
     return () => (
       <div>
-        <nav>nav</nav>
+        <Navbar>
+          {{
+            title:()=>'山竹记账',
+            icon:()=><Icon name='menu' class={s.navIcon}/>
+          }}
+        </Navbar>
         <Center class={s.icon_wrapper}>
           <Icon name="money" class={s.icon}/>
         </Center>
 
         <div class={s.button_wrapper}>
-          <Button class={s.button} onClick={onClick}>button</Button>
+          <Button class={s.button} onClick={onClick}>开始记账</Button>
         </div>
 
         <div>
