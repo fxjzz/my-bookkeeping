a2a138d77e616247ca69109e6ed4960546e079c3
fxjzz <1042183935@qq.com>
2022-12-02 20:06:57 +0800

    创建 MainLayout 组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,5 +1,6 @@
 import { computed, defineComponent, ref, Transition } from "vue";
 import { RouterLink } from "vue-router";
+import { MainLayout } from "../layout/MainLayout";
 import { Button } from "../shared/Button";
 import { Center } from "../shared/Center";
 import { FloatButton } from "../shared/FloatButton";
@@ -18,43 +19,43 @@ export const StartPage = defineComponent({
       overlayVisible.value = !overlayVisible.value;
     };
     return () => (
-      <>
-        <div>
-          <Navbar>
-            {{
-              title: () => "山竹记账",
-              icon: () => (
-                <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
-              ),
-            }}
-          </Navbar>
-          <Center class={s.icon_wrapper}>
-            <Icon name="money" class={s.icon} />
-          </Center>
-          <div class={s.button_wrapper}>
-            <RouterLink to="/items">
-              <Button class={s.button} onClick={onClick}>
-                开始记账
-              </Button>
-            </RouterLink>
-          </div>
+      <MainLayout>
+        {{
+          title: () => "山竹记账",
+          icon: () => (
+            <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
+          ),
+          default: () => (
+            <>
+              <Center class={s.icon_wrapper}>
+                <Icon name="money" class={s.icon} />
+              </Center>
+              <div class={s.button_wrapper}>
+                <RouterLink to="/items">
+                  <Button class={s.button} onClick={onClick}>
+                    开始记账
+                  </Button>
+                </RouterLink>
+              </div>
 
-          <RouterLink to="/items/create">
-            <FloatButton />
-          </RouterLink>
+              <RouterLink to="/items/create">
+                <FloatButton />
+              </RouterLink>
 
-          <Transition
-            enterFromClass={s.slide_fade_enter_from}
-            enterActiveClass={s.slide_fade_enter_active}
-            leaveToClass={s.slide_fade_leave_to}
-            leaveActiveClass={s.slide_fade_leave_active}
-          >
-            {overlayVisible.value && (
-              <Overlay onClose={() => (overlayVisible.value = false)} />
-            )}
-          </Transition>
-        </div>
-      </>
+              <Transition
+                enterFromClass={s.slide_fade_enter_from}
+                enterActiveClass={s.slide_fade_enter_active}
+                leaveToClass={s.slide_fade_leave_to}
+                leaveActiveClass={s.slide_fade_leave_active}
+              >
+                {overlayVisible.value && (
+                  <Overlay onClose={() => (overlayVisible.value = false)} />
+                )}
+              </Transition>
+            </>
+          ),
+        }}
+      </MainLayout>
     );
   },
 });
