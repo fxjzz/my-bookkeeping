bc4d734664b0afa0629019ff829b1d9ca528a595
fxjzz <1042183935@qq.com>
2022-12-05 16:23:59 +0800

    重写input Pad 布局

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,4 +1,4 @@
-import { DatetimePicker, Popup } from "vant";
+import { DatetimePicker, Popup ,NumberKeyboard  } from "vant";
 import { defineComponent, PropType, ref } from "vue";
 import { Icon } from "../../shared/Icon";
 import { time } from "../../shared/time";
@@ -12,43 +12,47 @@ export const InputPad = defineComponent({
   setup: (props, context) => {
     const refDate = ref<Date>();
     const now = new Date();
+    const refAmount=ref('')
+    const appendText = (n: number | string) => refAmount.value += n.toString()
     const buttons = [
-      { text: "7", onClick: () => {} },
-      { text: "8", onClick: () => {} },
-      { text: "9", onClick: () => {} },
-      { text: "清空", onClick: () => {} },
-      { text: "4", onClick: () => {} },
-      { text: "5", onClick: () => {} },
-      { text: "6", onClick: () => {} },
-      { text: "+", onClick: () => {} },
-      { text: "1", onClick: () => {} },
-      { text: "2", onClick: () => {} },
-      { text: "3", onClick: () => {} },
-      { text: "-", onClick: () => {} },
-      { text: "0", onClick: () => {} },
-      { text: ".", onClick: () => {} },
-      { text: "删", onClick: () => {} },
-      { text: "ok", onClick: () => {} },
-    ];
-    const refShowPop = ref(false)
+        {text: '1', onClick: () => { appendText(1) }},
+        { text: '2', onClick: () => { appendText(2) } },
+        { text: '3', onClick: () => { appendText(3) } },
+        { text: '4', onClick: () => { appendText(4) } },
+        { text: '5', onClick: () => { appendText(5) } },
+        { text: '6', onClick: () => { appendText(6) } },
+        { text: '7', onClick: () => { appendText(7) } },
+        { text: '8', onClick: () => { appendText(8) } },
+        { text: '9', onClick: () => { appendText(9) } },
+        { text: '.', onClick: () => { appendText('.') } },
+        { text: '0', onClick: () => { appendText(0) } },
+        { text: '清空', onClick: () => { } },
+        { text: '提交', onClick: () => { } },
+      ]
+    const refDatePickerVisible = ref(false);
+    const showDatePicker = () => (refDatePickerVisible.value = true);
+    const hideDatePicker = () => (refDatePickerVisible.value = false);
+    const setDate = (date: Date) => {
+      refDate.value = date;
+      hideDatePicker();
+    };
     return () => (
       <>
         <div class={s.dateAndAmount}>
           <span class={s.date}>
             <Icon name="note" class={s.icon} />
-            <span onClick={() => (refShowPop.value = true)}>
-              {time(refDate.value).format()}
-            </span>
-            <Popup position="bottom" v-model:show={refShowPop.value}>
+            <span onClick={showDatePicker}>{time(refDate.value).format()}</span>
+            <Popup position="bottom" v-model:show={refDatePickerVisible.value}>
               <DatetimePicker
-                v-model={refDate.value}
+                value={refDate.value}
                 type="date"
                 title="选择年月日"
-                onConfirm={() => (refShowPop.value = false)}
+                onConfirm={setDate}
+                onCancel={hideDatePicker}
               />
             </Popup>
           </span>
-          <span class={s.amount}>199.12</span>
+          <span class={s.amount}>{refAmount.value}</span>
         </div>
         <div class={s.buttons}>
           {buttons.map((button) => (
