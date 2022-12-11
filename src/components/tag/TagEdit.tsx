f41103cd533410f75fdc01f5c07640ae12553f40
fxjzz <1042183935@qq.com>
2022-12-11 16:37:13 +0800

    完成TagEdit 页面 样式

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -1,4 +1,10 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, reactive } from 'vue';
+import { MainLayout } from '../../layout/MainLayout';
+import { Button } from '../../shared/Button';
+import { Icon } from '../../shared/Icon';
+import { Rules, validate } from '../../shared/validate';
+import { TagForm } from './TagForm';
+import s from './Tag.module.scss';
 export const TagEdit = defineComponent({
   props: {
     name: {
@@ -6,8 +12,40 @@ export const TagEdit = defineComponent({
     }
   },
   setup: (props, context) => {
+    const formData = reactive({
+      name:'',
+      sign:''
+    })
+    const errors = reactive<{[k in keyof typeof formData]?:string[]}>({})
+    const onSubmit = (e:Event)=>{
+      const rules:Rules<typeof formData>=[
+        {key:'name',type:'required',message:'必填'},
+        {key:'name',type:'pattern',message:'只能填 1 到 4 个字符',regex:/^.{1,4}$/},
+        {key:'sign',type:'required',message:'请选中一个emoji'}
+      ]
+      Object.assign(errors, {
+        name: undefined,
+        sign: undefined
+      })
+      Object.assign(errors, validate(formData, rules))
+      e.preventDefault()
+    }
     return () => (
-      <div>edit</div>
+      <MainLayout>
+        {{
+          title:()=>'编辑标签',
+          icon:()=><Icon name='larrow' onClick={()=>{}}/>,
+          default:()=><>
+          <TagForm />
+          <div class={s.actions}>
+            <Button level='danger' class={s.removeTags} onClick={() => { }}>删除标签</Button>
+            <Button level='danger' class={s.removeTagsAndItems} onClick={() => { }}>删除标签和记账</Button>
+          </div>
+          </>
+        }}
+      </MainLayout>
     )
+    
+
   }
 })
