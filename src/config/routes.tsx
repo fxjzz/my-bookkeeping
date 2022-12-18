eb3b35f9e7a412af1a3e7afbc34512b4e263b85c
fxjzz <1042183935@qq.com>
2022-12-18 11:26:56 +0800

    创建统计图表页面

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -16,30 +16,57 @@ import { TagPage } from "../views/TagPage";
 import { TagCreate } from "../components/tag/TagCreate";
 import { TagEdit } from "../components/tag/TagEdit";
 import { SignInPage } from "../views/SignInPage";
+import { StatisticsPage } from "../views/StatisticsPage";
 
-
-export const routes:RouteRecordRaw[] = [
-    {path:'/',redirect:'/welcome'},
-    {path:'/welcome',component:Welcome,
-    children:[
-        {path:'',redirect:'/welcome/1'},
-        {path:'1',name:"Welcome1",components:{main:First,footer:FirstActions}},
-        {path:'2',name:"Welcome2",components:{main:Second,footer:SecondActions}},
-        {path:'3',name:"Welcome3",components:{main:Third,footer:ThirdActions}},
-        {path:'4',name:"Welcome4",components:{main:Forth,footer:ForthActions}},
-    ]},
-    {path:'/start',component:StartPage},
-    {path:'/items',component:ItemPage,
-    children:[
-        {path:'',component:ItemList},
-        {path:'create',component:ItemCreate}
-    ]},
-    {
-        path:'/tags',component:TagPage,
-        children:[
-            {path:'create',component:TagCreate},
-            {path:':id/edit',component:TagEdit}
-        ]
-    },
-    {path:'/sign_in',component:SignInPage}
-]
+export const routes: RouteRecordRaw[] = [
+  { path: "/", redirect: "/welcome" },
+  {
+    path: "/welcome",
+    component: Welcome,
+    children: [
+      { path: "", redirect: "/welcome/1" },
+      {
+        path: "1",
+        name: "Welcome1",
+        components: { main: First, footer: FirstActions },
+      },
+      {
+        path: "2",
+        name: "Welcome2",
+        components: { main: Second, footer: SecondActions },
+      },
+      {
+        path: "3",
+        name: "Welcome3",
+        components: { main: Third, footer: ThirdActions },
+      },
+      {
+        path: "4",
+        name: "Welcome4",
+        components: { main: Forth, footer: ForthActions },
+      },
+    ],
+  },
+  { path: "/start", component: StartPage },
+  {
+    path: "/items",
+    component: ItemPage,
+    children: [
+      { path: "", component: ItemList },
+      { path: "create", component: ItemCreate },
+    ],
+  },
+  {
+    path: "/tags",
+    component: TagPage,
+    children: [
+      { path: "create", component: TagCreate },
+      { path: ":id/edit", component: TagEdit },
+    ],
+  },
+  { path: "/sign_in", component: SignInPage },
+  {
+    path: "/statistics",
+    component: StatisticsPage,
+  },
+];
---
+++ src/layout/TimeTabsLayout.tsx
@@ -0,0 +1,142 @@
+import { Overlay as VOverlay } from "vant";
+import { defineComponent, PropType, reactive, ref, Transition } from "vue";
+import { ItemSummary } from "../components/item/ItemSummary";
+import { Form, FormItem } from "../shared/Form";
+import { Icon } from "../shared/Icon";
+import { Overlay } from "../shared/Overlay";
+import { Tab, Tabs } from "../shared/Tabs";
+import { Time } from "../shared/time";
+import s from "./TimeTabsLayout.module.scss";
+import { MainLayout } from "./MainLayout";
+const demo = defineComponent({
+  props: {
+    startDate: {
+      type: String as PropType<string>,
+      required: true,
+    },
+    endDate: {
+      type: String as PropType<string>,
+      required: true,
+    },
+  },
+});
+export const TimeTabsLayout = defineComponent({
+  props: {
+    component:{
+        type:Object as PropType<typeof demo>,
+        required:true
+    }
+  },
+  setup: (props, context) => {
+    const overlayVisible = ref(false);
+    const onClickMenu = () => {
+      overlayVisible.value = !overlayVisible.value;
+    };
+    const refSelected = ref("本月");
+    const time = new Time();
+    const customTime = reactive({
+      start: new Time().format(),
+      end: new Time().format(),
+    });
+    const timeList = [
+      {
+        start: time.firstDayOfMonth(),
+        end: time.lastDayOfMonth(),
+      },
+      {
+        start: time.add(-1, "month").firstDayOfMonth(),
+        end: time.add(-1, "month").lastDayOfMonth(),
+      },
+      {
+        start: time.firstDayOfYear(),
+        end: time.lastDayOfYear(),
+      },
+    ];
+    const refOverlayVisible = ref(false);
+    const onSubmitCustomTime = (e: Event) => {
+      e.preventDefault();
+      refOverlayVisible.value = false;
+    };
+    const onSelect = (value: string) => {
+      if (value === "自定义时间") {
+        refOverlayVisible.value = true;
+      }
+    };
+    return () => (
+      <MainLayout class={s.wrapper}>
+        {{
+          title: () => "山竹记账",
+          icon: () => <Icon name="menu" class={s.icon} onClick={onClickMenu} />,
+          default: () => (
+            <>
+              <Tabs
+                v-model:selected={refSelected.value}
+                onUpdate:selected={onSelect}
+              >
+                <Tab name="本月">
+                  <props.component
+                    startDate={timeList[0].start.format()}
+                    endDate={timeList[0].end.format()}
+                  />
+                </Tab>
+                <Tab name="上月">
+                  <props.component
+                    startDate={timeList[1].start.format()}
+                    endDate={timeList[1].end.format()}
+                  />
+                </Tab>
+                <Tab name="今年">
+                  <props.component
+                    startDate={timeList[2].start.format()}
+                    endDate={timeList[2].end.format()}
+                  />
+                </Tab>
+                <Tab name="自定义时间">
+                  <props.component
+                    startDate={customTime.start}
+                    endDate={customTime.end}
+                  />
+                </Tab>
+              </Tabs>
+              <VOverlay show={refOverlayVisible.value} class={s.overlay}>
+                <div class={s.overlay_inner}>
+                  <header>请选择时间</header>
+                  <main>
+                    <Form onSubmit={onSubmitCustomTime}>
+                      <FormItem
+                        label="开始时间"
+                        v-model={customTime.start}
+                        type="date"
+                      />
+                      <FormItem
+                        label="结束时间"
+                        v-model={customTime.end}
+                        type="date"
+                      />
+                      <FormItem>
+                        <div class={s.actions}>
+                          <button type="button">取消</button>
+                          <button type="submit">确认</button>
+                        </div>
+                      </FormItem>
+                    </Form>
+                  </main>
+                </div>
+              </VOverlay>
+              <Transition
+                enterFromClass={s.slide_fade_enter_from}
+                enterActiveClass={s.slide_fade_enter_active}
+                leaveToClass={s.slide_fade_leave_to}
+                leaveActiveClass={s.slide_fade_leave_active}
+              >
+                {overlayVisible.value && (
+                  <Overlay onClose={() => (overlayVisible.value = false)} />
+                )}
+              </Transition>
+            </>
+          ),
+        }}
+      </MainLayout>
+    );
+  },
+});
