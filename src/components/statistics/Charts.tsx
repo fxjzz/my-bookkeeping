a4b9f153f824683f8ba6d90761355ceafa2acb3c
fxjzz <1042183935@qq.com>
2022-12-18 15:49:15 +0800

    删除无用代码

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,7 +1,6 @@
-import { computed, defineComponent,onMounted , PropType, reactive, ref } from 'vue';
+import { defineComponent,PropType, ref } from 'vue';
 import { FormItem } from '../../shared/Form';
 import s from './Charts.module.scss';
-import * as echarts from 'echarts'
 import { Bars } from './Bars';
 import { LineChart } from './LineChart';
 import { PieChart } from './PieChart';
@@ -18,64 +17,6 @@ export const Charts = defineComponent({
   },
   setup: (props, context) => {
     const category = ref('expenses')
-    const refDiv = ref<HTMLDivElement>()
-    const refDiv2 = ref<HTMLDivElement>()
-    
-    onMounted(()=>{
-      if(refDiv.value===undefined) return
-      var myChart = echarts.init(refDiv.value)
-      myChart.setOption({
-        grid: [
-          { left: 0, top: 0, right: 0, bottom: 20 }
-        ],
-        xAxis: {
-          type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [
-          {
-            data: [150, 230, 224, 218, 135, 147, 260],
-            type: 'line'
-          }
-        ]
-      })
-    })
-    onMounted(() => {
-      if (refDiv2.value === undefined) { return }
-      // 基于准备好的dom，初始化echarts实例
-      var myChart = echarts.init(refDiv2.value);
-      // 绘制图表
-      const option = {
-        grid: [
-          { left: 0, top: 0, right: 0, bottom: 20 }
-        ],
-        series: [
-          {
-            name: 'Access From',
-            type: 'pie',
-            radius: '50%',
-            data: [
-              { value: 1048, name: 'Search Engine' },
-              { value: 735, name: 'Direct' },
-              { value: 580, name: 'Email' },
-              { value: 484, name: 'Union Ads' },
-              { value: 300, name: 'Video Ads' }
-            ],
-            emphasis: {
-              itemStyle: {
-                shadowBlur: 10,
-                shadowOffsetX: 0,
-                shadowColor: 'rgba(0, 0, 0, 0.5)'
-              }
-            }
-          }
-        ]
-      };
-      myChart.setOption(option);
-    })
     return () => (
       <div class={s.wrapper}>
         <FormItem label='类型' type='select' options={[
