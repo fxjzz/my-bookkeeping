d5f16e199b0f9ff227e36bf1b91c175f5341f975
fxjzz <1042183935@qq.com>
2023-01-11 15:06:16 +0800

    设置全局守卫

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,22 @@ import {createRouter} from 'vue-router'
 import { routes } from './config/routes'
 import { history } from './shared/history'
 import '@svgstore'
+import { http } from './shared/Http'
 
-const router = createRouter({
-    history,
-    routes, 
-  })
+const router = createRouter({history,routes, })
+
+const promise = http.get('/me')
+router.beforeEach(async (to,from)=>{
+  if(to.path==='/' || to.path.startsWith('/welcome') || to.path.startsWith('/sign_in') || to.path.startsWith('/start')){
+    return true
+  } else {
+    const path = await promise.then(
+      ()=>true,
+      ()=>'/sign_in?return_to=' + to.path
+    )
+    return path 
+  }
+})
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
