5686005d3158875acfdfe79c9d88cb1050813a22
lzxzz <1042183935@qq.com>
2023-02-28 10:16:16 +0800

    封装useTags

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -6,39 +6,43 @@ type Mock = (config: AxiosRequestConfig) => [number, any];
 faker.setLocale("zh_CN");
 
 export const mockSession: Mock = (config) => {
-    return [200,{
-        jwt:faker.random.word()
-    }]
+  return [200, {
+    jwt: faker.random.word()
+  }]
 };
 
-export const mockTagIndex:Mock =(config)=>{
-    const {kind , page} = config.params
-    const per_page = 25
-    const count = 26
-    let id = 0
-    const createId = () => {
-      id += 1
-      return id
-    }
-    const createPager = (page = 1) =>({
-      page,per_page,count
-    })
-    const createTag = (n = 1, attrs?: any) =>
-      Array.from({ length: n }).map(() => ({
-        id: createId(),
-        name: faker.lorem.word(),
-        sign: faker.internet.emoji(),
-        kind: config.params.kind,
-        ...attrs
-      }))
-    const createBody = (n=1,attrs?:any)=>({
-      resources:createTag(n),pager:createPager(page)
-    })
-    if(kind==='expenses' && (page === 1 || !page)){
-      return [200,createBody(25)]
-    }else if(kind==='expenses' && page ===2){
-      return [200,createBody(1)]  
-    }else{
-      return [200,{resources:createTag(20)}]
-    }
+export const mockTagIndex: Mock = (config) => {
+  const { kind, page } = config.params
+  const per_page = 25
+  const count = 26
+  let id = 0
+  const createId = () => {
+    id += 1
+    return id
+  }
+  const createPager = (page = 1) => ({
+    page, per_page, count
+  })
+  const createTag = (n = 1, attrs?: any) =>
+    Array.from({ length: n }).map(() => ({
+      id: createId(),
+      name: faker.lorem.word(),
+      sign: faker.internet.emoji(),
+      kind: config.params.kind,
+      ...attrs
+    }))
+  const createBody = (n = 1, attrs?: any) => ({
+    resources: createTag(n), pager: createPager(page)
+  })
+  if (kind === 'expenses' && (!page || page === 1)) {
+    return [200, createBody(25)]
+  } else if (kind === 'expenses' && page === 2) {
+    return [200, createBody(1)]
+  } else if (kind === 'income' && (!page || page === 1)) {
+    return [200, createBody(25)]
+  } else if (kind === 'income' && page === 2) {
+    return [200, createBody(1)]
+  } else {
+    return [200, { resources: createTag(1) }]
+  }
 }
