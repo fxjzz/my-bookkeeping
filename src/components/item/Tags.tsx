3e0b3af5bea1928afc3a1ae11256c53d1723c595
lzxzz <1042183935@qq.com>
2023-03-01 20:13:58 +0800

    添加长按事件

--- src/components/item/Tags.tsx
+++ src/components/item/Tags.tsx
@@ -1,52 +1,87 @@
-import { defineComponent, PropType } from 'vue';
-import { RouterLink } from 'vue-router';
-import { Button } from '../../shared/Button';
-import { http } from '../../shared/Http';
-import { Icon } from '../../shared/Icon';
-import { useTags } from '../../shared/useTags';
-import s from './Tags.module.scss';
+import { defineComponent, PropType, ref } from "vue";
+import { RouterLink } from "vue-router";
+import { Button } from "../../shared/Button";
+import { http } from "../../shared/Http";
+import { Icon } from "../../shared/Icon";
+import { useTags } from "../../shared/useTags";
+import s from "./Tags.module.scss";
 export const Tags = defineComponent({
   props: {
     kind: {
       type: String as PropType<string>,
-      required: true
+      required: true,
     },
-    selected: Number
+    selected: Number,
   },
-  emits: ['update:selected'],
+  emits: ["update:selected"],
   setup: (props, context) => {
     const { tags, hasMore, fetchTags } = useTags((page) => {
-      return http.get<Resources<Tag>>('/tags', {
+      return http.get<Resources<Tag>>("/tags", {
         kind: props.kind,
         page: page + 1,
-        _mock: 'tagIndex'
-      })
-    })
+        _mock: "tagIndex",
+      });
+    });
     const onSelect = (tag: Tag) => {
-      context.emit('update:selected', tag.id)
-    }
-    return () => <>
-      <div class={s.tags_wrapper}>
-        <RouterLink to={`/tags/create?kind=${props.kind}`} class={s.tag}>
-          <div class={s.sign}>
-            <Icon name="add" class={s.createTag} />
-          </div>
-          <div class={s.name}>新增</div>
-        </RouterLink>
-        {tags.value.map(tag =>
-          <div class={[s.tag, props.selected === tag.id ? s.selected : '']}
-            onClick={() => onSelect(tag)}>
-            <div class={s.sign}>{tag.sign}</div>
-            <div class={s.name}>{tag.name}</div>
-          </div>
-        )}
-      </div>
-      <div class={s.more}>
-        {hasMore.value ?
-          <Button class={s.loadMore} onClick={fetchTags}>加载更多</Button> :
-          <span class={s.noMore}>没有了...</span>
-        }
-      </div>
-    </>
-  }
-})
+      context.emit("update:selected", tag.id);
+    };
+    const timer = ref<number>();
+    const currentTag = ref<HTMLDivElement>();
+    const onLongPress = () => {
+      console.log("触发长按");
+    };
+    const onTouchStart = (e: TouchEvent) => {
+      currentTag.value = e.currentTarget as HTMLDivElement;
+      timer.value = setTimeout(() => {
+        onLongPress();
+      }, 500);
+    };
+    const onTouchEnd = (e: TouchEvent) => {
+      clearTimeout(timer.value);
+    };
+    const onTouchMove = (e: TouchEvent) => {
+      const pointedEl = document.elementFromPoint(
+        e.touches[0].clientX,
+        e.touches[0].clientY
+      );
+      if (
+        currentTag.value !== pointedEl &&
+        currentTag.value?.contains(pointedEl) === false
+      ) {
+        clearTimeout(timer.value);
+      }
+    };
+    return () => (
+      <>
+        <div class={s.tags_wrapper} onTouchmove={onTouchMove}>
+          <RouterLink to={`/tags/create?kind=${props.kind}`} class={s.tag}>
+            <div class={s.sign}>
+              <Icon name="add" class={s.createTag} />
+            </div>
+            <div class={s.name}>新增</div>
+          </RouterLink>
+          {tags.value.map((tag) => (
+            <div
+              class={[s.tag, props.selected === tag.id ? s.selected : ""]}
+              onClick={() => onSelect(tag)}
+              onTouchstart={onTouchStart}
+              onTouchend={onTouchEnd}
+            >
+              <div class={s.sign}>{tag.sign}</div>
+              <div class={s.name}>{tag.name}</div>
+            </div>
+          ))}
+        </div>
+        <div class={s.more}>
+          {hasMore.value ? (
+            <Button class={s.loadMore} onClick={fetchTags}>
+              加载更多
+            </Button>
+          ) : (
+            <span class={s.noMore}>没有了...</span>
+          )}
+        </div>
+      </>
+    );
+  },
+});
