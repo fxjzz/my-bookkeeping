32c86fb8bbb08309ec9f90132bcdcde7914a043b
lzxzz <1042183935@qq.com>
2023-03-01 16:41:36 +0800

    完成创建标签功能

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,41 +1,17 @@
-import { defineComponent, PropType, reactive } from 'vue';
+import { defineComponent } from 'vue';
 import { MainLayout } from '../../layout/MainLayout';
 import { BackIcon } from '../../shared/BackIcon';
-import { Button } from '../../shared/Button';
-import { EmojiSelect } from '../../shared/EmojiSelect';
-import { Icon } from '../../shared/Icon';
-import { Rules, validate } from '../../shared/validate';
-import s from './Tag.module.scss';
 import { TagForm } from './TagForm';
 
 export const TagCreate = defineComponent({
   setup: (props, context) => {
-    const formData = reactive({
-      name: '',
-      sign: ''
-    })
-    const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({})
-    const onSubmit = (e: Event) => {
-      const rules: Rules<typeof formData> = [
-        { key: 'name', type: 'required', message: '必填' },
-        { key: 'name', type: 'pattern', message: '只能填 1 到 4 个字符', regex: /^.{1,4}$/ },
-        { key: 'sign', type: 'required', message: '请选中一个emoji' }
-      ]
-      Object.assign(errors, {
-        name: undefined,
-        sign: undefined
-      })
-      Object.assign(errors, validate(formData, rules))
-      e.preventDefault()
-    }
-    return () => (
-      <MainLayout>{{
-        title: () => '新建标签',
-        icon: () => <BackIcon />,
-        default: () => (
-          <TagForm />
-        )
-      }}</MainLayout>
-    )
+    return () =>
+      <MainLayout>
+        {{
+          title: () => '新建标签',
+          icon: () => <BackIcon />,
+          default: () => <TagForm />
+        }}
+      </MainLayout>
   }
 })
