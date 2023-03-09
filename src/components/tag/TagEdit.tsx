2e214a1e3dfedf6c16e372e252d27ecb1a371442
lzxzz <1042183935@qq.com>
2023-03-09 15:30:06 +0800

    feat: 删除标签按钮

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -37,11 +37,8 @@ export const TagEdit = defineComponent({
             <>
               <TagForm id={id} />
               <div class={s.actions}>
-                <Button level="danger" class={s.removeTags} onClick={() => onDelete()}>
-                  删除标签
-                </Button>
                 <Button level="danger" class={s.removeTagsAndItems} onClick={() => onDelete({ withItems: true })}>
-                  删除标签和记录
+                  删除标签和记账
                 </Button>
               </div>
             </>
